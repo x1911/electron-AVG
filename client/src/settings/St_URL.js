c6909a5c9a3b25ed166e55408af982b336f5faf3
x1911 <bube_icq@yahoo.de>
2023-07-09 11:45:03 +0800

    test1

--- client/src/settings/St_URL.js
+++ client/src/settings/St_URL.js
@@ -3,5 +3,7 @@ export default Object.freeze({
     JsonFolder: 'Assets/Resources/JsonData/',
     CharacterFolder: 'Assets/Resources/Character/',
     // 徐庶-布衣-普通
-    BGFolder: 'Assets/Resources/Background/'
+    BGFolder: 'Assets/Resources/Background/',
+    MusicFolder: 'Assets/Resources/Music/',
+    SoundFolder: 'Assets/Resources/Sound/',
 })
